305bd23828b39382cee733f3646d7b86bb81f6cd
Rood Jery Pierre <rood_pierre@ucne.edu.do>
2021-05-30 15:10:26 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1erParcial.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\1erProductos.db");
+        }
+    }
+}
